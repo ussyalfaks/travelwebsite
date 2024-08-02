dfcc27ce3ba4684911f6eb53897b8806da4a43a5
ussyalfaks <usmanalfaki@gmail.com>
2024-08-02 23:26:10 +0100

    Travel website

--- travelsite/src/App.jsx
+++ travelsite/src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import './App.css'
+import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import "./index.css";
+import Navbar from "./components/Navbar"
+import Home from "./components/pages/home";
+
 
 function App() {
-
   return (
-    <>
-     
-    </>
-  )
+    <div className="App ">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ travelsite/src/components/navbar.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setMenuOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  // UseEffect to close the menu on location change
+  useEffect(() => {
+    setMenuOpen(false);
+  },);
+
+  // UseEffect to handle scroll and history changes
+  useEffect(() => {
+    // Disable scroll when menu is open
+    if (menuOpen) {
+      document.body.classList.add('overflow-hidden');
+    } else {
+      document.body.classList.remove('overflow-hidden');
+    }
+    return () => {
+      document.body.classList.remove('overflow-hidden');
+    };
+  }, [menuOpen]);
+  
+  return (
+    <header className={` flex justify-between items-center  ${menuOpen ? 'h-screen max-md:flex max-md:items-start bg-slate-400' : ''} `}>
+      <nav className={`flex justify-between w-full h-full  px-10 pt-8 pb-3 mb-0 ${menuOpen? 'flex flex-col h-full ':''}`}>
+        <div className={`mb-4 md:mb-0 flex justify-start  ${menuOpen ? 'md:text-center text-white' : ''}`}>
+          <Link to="/" className="Logo font-mono">
+            DEVELOPEDBYUSMAN
+          </Link>
+        </div>
+        <div className={`md:flex md:items-center items-center ${menuOpen ? 'h-screen flex justify-center items-center' : ''}`}>
+          <ul className={`flex ${menuOpen ? 'flex flex-col items-center justify-center ' : 'max-md:hidden'} `}>
+            <li className="md:mx-4 my-2 md:my-0">
+              <NavLink to="/" 
+              // className='hidden'
+              >Home</NavLink>
+            </li>
+            {/* <li className="md:mx-4 my-2 md:my-0">
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li className="md:mx-4 my-2 md:my-0">
+              <NavLink to="/contact">Collection</NavLink>
+            </li> */}
+            <li className="">
+              <NavLink to="#"
+               className=" bg-[#437D9D] p-3 rounded-2xl hover:border-blue-700 hover:border-2 text-white">
+                Sign up
+                </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className={` ${menuOpen? 'p-7 px-4' :'md:hidden px-4'}`}>
+          <FontAwesomeIcon
+            icon={faBars}
+            onClick={() => setMenuOpen(!menuOpen)}
+            className="text-2xl cursor-pointer "
+          />
+        </div>
+    </header>
+  );
+};
+
+export default Navbar;
