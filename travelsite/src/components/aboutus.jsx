b1ca7045bbb32dcee5c81cc374eca434cc70b5a2
ussyalfaks <usmanalfaki@gmail.com>
2024-08-04 17:27:27 +0100

    about us section

--- travelsite/src/components/aboutus.jsx
+++ travelsite/src/components/aboutus.jsx
@@ -7,6 +7,10 @@ function aboutus() {
     <div>
         <h1>Why We Are The Best</h1>
         <p>Unmatched travel website designs for seamless exploration</p>
+
+        <div className='flex'>
+
+        </div>
     </div>
     </>
   )
