3915e4e4fff7775a8f9cb77a15dc29b102f54e45
ussyalfaks <usmanalfaki@gmail.com>
2024-08-04 17:33:46 +0100

    about sec

--- travelsite/src/components/aboutus.jsx
+++ travelsite/src/components/aboutus.jsx
@@ -4,12 +4,12 @@ function aboutus() {
   return (
     <>
     
-    <div>
+    <div className='flex justify-center items-center flex-col'>
         <h1>Why We Are The Best</h1>
         <p>Unmatched travel website designs for seamless exploration</p>
 
-        <div className='flex'>
-
+        <div className='flex justify-center items-center'>
+        
         </div>
     </div>
     </>
