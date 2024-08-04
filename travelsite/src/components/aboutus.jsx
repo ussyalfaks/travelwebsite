8d8f2df4da0393553c7a2405baaa7776e704fdd2
ussyalfaks <usmanalfaki@gmail.com>
2024-08-04 17:26:03 +0100

    about us section

---
+++ travelsite/src/components/aboutus.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function aboutus() {
+  return (
+    <>
+    
+    <div>
+        <h1>Why We Are The Best</h1>
+        <p></p>
+    </div>
+    </>
+  )
+}
+
+export default aboutus
